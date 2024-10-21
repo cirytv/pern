e12bc27fc0afbaca0d2d848f8b5cee2a7b9f154b
ciry <>
2024-10-21 04:56:44 -0700

    Code Coverage PATCH Integration Testing + Jest Mock forcing errors for Unit Testing server.test.ts

--- api/src/__test__/server.test.ts
+++ api/src/__test__/server.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
-import server from '../server'
+import server, { connectDB } from '../server'
+import db from '../config/db' // estancia sequelize
 
 describe('GET /api', () => {
   test('should send back a json response', async () => {
@@ -11,5 +12,21 @@ describe('GET /api', () => {
 
     expect(res.status).not.toBe(404)
     expect(res.body.msg).not.toBe('From api')
+  })
+})
+
+jest.mock('../config/db')
+describe('connectDB', () => {
+  test('handle database connection error', async () => {
+    jest
+      .spyOn(db, 'authenticate')
+      .mockRejectedValueOnce(new Error('Hubo un error al conectar a la BD'))
+    const consoleSpy = jest.spyOn(console, 'log')
+
+    await connectDB()
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      expect.stringContaining('Hubo un error al conectar a la BD')
+    )
   })
 })
--- api/src/server.ts
+++ api/src/server.ts
@@ -4,13 +4,13 @@ import router from './router'
 import db from './config/db'
 
 // Conectar a base de datos
-async function connectDB() {
+export async function connectDB() {
   try {
     await db.authenticate()
     db.sync()
     // console.log(colors.blue('Conexión exitosa a la BD'))
   } catch (error) {
-    console.log(error)
+    // console.log(error)
     console.log(colors.red.bold('Hubo un error al conectar a la BD'))
   }
 }
