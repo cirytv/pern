3c135850064c84c31c1ace088d77dc2cc94f38ab
ciry <>
2024-11-03 19:47:46 -0800

    client

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,6 +5,8 @@ import router from './router'
 import db from './config/db'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerUiOptions } from './config/swagger'
+import cors, { CorsOptions } from 'cors'
+import morgan from 'morgan'
 
 // Conectar a base de datos
 export async function connectDB() {
@@ -21,10 +23,22 @@ connectDB()
 
 // Instancia de express
 const server = express()
+// Permitir conexiones - cors
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de CORS'))
+    }
+  },
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
 
+server.use(morgan('dev')) // use morgan for request details
 server.use('/api/products', router)
 
 // Swagger Docs Router
